46f3a600d30c97fc863181a2e618228509cc5b35
kallyas <kallyasmedia@gmail.com>
2022-08-18 12:18:22 +0300

    ch: create lesson one

---
+++ week1/lesson_one.md
@@ -0,0 +1,10 @@
+# CIT PYTHON CLASS WEEK ONE
+
+### Introduction to python
+what is python?
+> Python is an interpreted, object-oriented, high-level programming language with dynamic semantics
+
+### First program
+```python
+print("Hello world")
+```
---
+++ week1/lesson_one.py
@@ -0,0 +1,33 @@
+print("Hello world")
+
+# python code structure
+
+# Variables
+my_name = "Iden"
+my_name_is = "Iden"
+
+# Data types
+# Strings
+all_chars = "A, B, C, D.....Z"
+my_name_is = "Iden"
+
+# Numbers/Integers
+my_age = 12
+all_numbers = 0, 1, 2, 3
+
+# Floating numbers
+my_weight = 12.5
+
+# Boolean True/False
+is_raining = True
+
+# None
+lunch = None
+
+
+my_name = "Iden"
+my_age = 12
+my_weight = 12.5
+
+# My name is Iden, I am 12 years old and I weigh 12.5kgs
+print("My name is " + my_name + ", I am " + str(my_age) + " years old")
