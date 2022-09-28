f1bef0494fc45fc3027fd848c44893f68084b61a
kallyas <kallyasmedia@gmail.com>
2022-09-28 18:52:40 +0300

    ch: add code

---
+++ week6/data_structures_and_algorithms/algorithms/bubble_sort/bubble.py
@@ -0,0 +1,69 @@
+# bubble sort using a while loop
+
+def bubble_sort(a):
+    n = len(a)
+    while n > 1:
+        i = 1
+        while i < n:
+            if a[i-1] > a[i]:
+                a[i-1], a[i] = a[i], a[i-1]
+            i += 1
+        n -= 1
+    return a
+
+
+# optimized bubble sort using a while loop by adding a swap flag
+def bubble_sort_opt(a):
+    n = len(a)
+    while n > 1:
+        i = 1
+        swap = False
+        while i < n:
+            if a[i-1] > a[i]:
+                a[i-1], a[i] = a[i], a[i-1]
+                swap = True
+            i += 1
+        if not swap:
+            break
+        n -= 1
+    return a
+
+
+# selection sort using a while loop
+def selection_sort(a):
+    n = len(a)
+    while n > 1:
+        i = 0
+        max_index = 0
+        while i < n:
+            if a[i] > a[max_index]:
+                max_index = i
+            i += 1
+        a[n-1], a[max_index] = a[max_index], a[n-1]
+        n -= 1
+    return a
+
+# selection sort using a for loop and while loop
+def selection_sort2(a):
+    n = len(a)
+    for i in range(n-1, 0, -1):
+        max_index = 0
+        j = 1
+        while j <= i:
+            if a[j] > a[max_index]:
+                max_index = j
+            j += 1
+        a[i], a[max_index] = a[max_index], a[i]
+    return a
+
+if __name__ == '__main__':
+    a = [5, 4, 3, 2, 1]
+    print(bubble_sort(a))
+    b = [5, 4, 3, 2, 1]
+    print(bubble_sort_opt(b))
+
+
+# Explanation
+# The bubble sort algorithm is a simple sorting algorithm that repeatedly steps through the list to be sorted, compares each pair of adjacent items and swaps them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted. The algorithm gets its name from the way smaller elements "bubble" to the top of the list. Because it only uses comparisons to operate on elements, it is a comparison sort. Although the algorithm is simple, most of the other sorting algorithms are more efficient for large lists.
+
+# The bubble sort makes multiple passes through a list. It compares adjacent items and exchanges those that are out of order. Each pass through the list places the next largest value in its proper place. In essence, each item "bubbles" up to the location where it belongs.
--- week6/data_structures_and_algorithms/algorithms/merge_sort_algorithm/merge_sort.py
+++ week6/data_structures_and_algorithms/algorithms/merge_sort_algorithm/merge_sort.py
@@ -30,6 +30,54 @@ def merge_sort(arr):
             j += 1
             k += 1
 
+
+def merge_sort_simpler(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort_simpler(left)
+        merge_sort_simpler(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        arr[k:] = left[i:] or right[j:]
+
+    return arr
+
+
+# merge sort shorter version
+def merge_sort_shorter(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort_shorter(left)
+        merge_sort_shorter(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            arr[k] = (left[i] if left[i] < right[j] else right[j])
+            i += (left[i] < right[j])
+            j += (left[i] >= right[j])
+            k += 1
+
+        arr[k:] = left[i:] or right[j:]
+
+    return arr
+
 if __name__ == "__main__":
     arr = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     merge_sort(arr)
